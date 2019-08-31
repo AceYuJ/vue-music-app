b19ee9200a69e7ba1ee5c0b25e6055ec81e05ca7
End <980483705@qq.com>
2019-08-31 12:18:38 +0800

    封装Tab组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
   </div>
 </template>
 
 <script>
 import MHeader from './views/m-header/m-header'
+import Tab from './views/tab/tab'
 export default {
   components:{
-    MHeader
+    MHeader,
+    Tab
   }
 }
 </script>
---
+++ src/views/tab/tab.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="tab">
+        <div class="tab-item">
+            <span>推荐</span>
+        </div>
+        <div class="tab-item">
+            <span>排行</span>
+        </div>
+        <div class="tab-item">
+            <span>歌手</span>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped lang="scss">
+    @import "@/common/scss/variable.scss";
+    .tab{
+        display: flex;
+        height: 44px;
+        line-height: 44px;
+        width: 100%;
+        background: $color-theme;
+        font-size:$font-size-medium;
+        .tab-item{
+            text-align: center;
+            flex: 1;//如果所有项目的flex-grow属性都为1，则它们将等分剩余空间（如果有的话）
+            color:$color-text-lm;
+        }
+    }
+</style>
