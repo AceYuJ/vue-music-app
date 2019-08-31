fbdfc92b824254370dd2383c4e3dd60d48c0c683
End <980483705@qq.com>
2019-08-31 12:15:28 +0800

    封装头部和Tab组件

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+  </div>
+</template>
+
+<script>
+import MHeader from './views/m-header/m-header'
+export default {
+  components:{
+    MHeader
+  }
+}
+</script>
+
+<style lang="scss">
+@import "common/scss/index.scss";
+#app{
+  position: fixed;
+  top: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+}
+</style>
---
+++ src/views/m-header/m-header.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="m-header">
+      <h1 class="text">VMUSIC</h1>
+      <div class="mine">
+        <i class="iconfont icon-list"></i>
+      </div>
+      <div class="search">
+        <i class="iconfont icon-search"></i>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+<style scoped lang="scss">
+@import "./../../common/scss/variable.scss";
+  .m-header{
+    position: relative;
+    width: 100%;
+    height: 44px;
+    text-align: center;
+    background: $color-theme;
+    .text{
+      line-height: 44px;
+      font-weight: bold;
+      color:$color-text-l;
+      letter-spacing: 3px;
+    }
+    .mine{
+      position: absolute;
+      top: 0;
+      left: 0;
+      .iconfont{
+        display: block;//将元素转换为块状，使得padding生效
+        padding: 11px;
+        font-size:22px;
+        color:$color-theme-l;
+      }
+    }
+    .search{
+      position: absolute;
+      top:0;
+      right: 0;
+      .iconfont{
+        display: block;
+        padding: 11px;
+        font-size: 22px;
+        color: $color-theme-l;
+      }
+    }
+  }
+</style>
